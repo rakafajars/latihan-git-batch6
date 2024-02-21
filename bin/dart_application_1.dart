0b8123da65505df4a15551a32431fcc760f74e39
Raka Fajar <rakafajar18@gmail.com>
2024-02-21 19:18:50 +0700

    Fix Bug Register Form Name

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -2,4 +2,7 @@ import 'package:dart_application_1/dart_application_1.dart' as dart_application_
 
 void main(List<String> arguments) {
   print('Hello Kampus Merdeka Batch 6: ${dart_application_1.calculate()}!');
+
+  var a = 'selesaoi';
 }
+
