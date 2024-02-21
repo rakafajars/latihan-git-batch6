f05ae913016a7d0788264ff0ea074baec4fbfced
Raka Fajar <rakafajar18@gmail.com>
2024-02-21 19:36:25 +0700

    Add Home Page & Detail Home

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -2,4 +2,10 @@ import 'package:dart_application_1/dart_application_1.dart' as dart_application_
 
 void main(List<String> arguments) {
   print('Hello 1: ${dart_application_1.calculate()}!');
+  perhitungan();
+}
+
+
+void perhitungan() {
+  print(1+1);
 }
